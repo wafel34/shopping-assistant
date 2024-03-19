8af1166a65abb703af582902d812df9fa90577c1
Paweł Marcinkowski <wafel34@gmail.com>
2024-03-19 09:00:56 +0100

    import angular material

--- apps/shopping-assistant/src/main.ts
+++ apps/shopping-assistant/src/main.ts
@@ -2,10 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from "@angular/core";
 import { ClientShellModule } from "@shopping-assistant/client-shell";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(ClientShellModule)
+    importProvidersFrom(ClientShellModule), provideAnimationsAsync()
   ]
 }).catch((err) =>
   console.error(err)
--- libs/client/shell/ui/layout/src/lib/layout/layout.component.ts
+++ libs/client/shell/ui/layout/src/lib/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbar } from "@angular/material/toolbar";
 
 @Component({
   selector: 'shopping-assistant-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbar],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
