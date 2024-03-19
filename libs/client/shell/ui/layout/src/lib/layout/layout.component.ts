3dafff67151e1f3292b1d9b652bf3e749201354b
Paweł Marcinkowski <wafel34@gmail.com>
2024-03-19 09:08:17 +0100

    import angular material

--- libs/client/shell/ui/layout/src/lib/layout/layout.component.ts
+++ libs/client/shell/ui/layout/src/lib/layout/layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbar } from "@angular/material/toolbar";
 
 @Component({
   selector: 'shopping-assistant-layout',
   standalone: true,
-  imports: [CommonModule, MatToolbar],
+  imports: [CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
