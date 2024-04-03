a2e5d312cc66f1916f451c58fecbeab962822069
Paweł Marcinkowski <wafel34@gmail.com>
2024-04-03 21:03:32 +0200

    add firestore emulator for dev mode

--- libs/client/shell/feature/src/lib/client-shell.module.ts
+++ libs/client/shell/feature/src/lib/client-shell.module.ts
@@ -1,10 +1,10 @@
-import { importProvidersFrom, NgModule } from '@angular/core';
+import { importProvidersFrom, isDevMode, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { clientShellRoutes } from './lib.routes';
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from "@angular/fire/firestore";
 
 @NgModule({
   imports: [CommonModule, RouterModule.forRoot(clientShellRoutes)],
@@ -18,8 +18,17 @@ import { getFirestore, provideFirestore } from "@angular/fire/firestore";
         "authDomain": "shopping-assistant-2dee7.firebaseapp.com",
         "messagingSenderId": "495643362914",
         "measurementId": "G-QYE4QP2VW0"
-      }))), importProvidersFrom(provideAuth(() => getAuth())),
-    importProvidersFrom(provideFirestore(() => getFirestore()))
+      }))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => {
+      const firestore = getFirestore();
+
+      if (isDevMode()) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080)
+      }
+
+      return firestore;
+    }))
   ]
 })
 export class ClientShellModule {
